1c4c278fd30d0056cc45bbfbae99bccd55882334
Harry Hong <harry851025@gmail.com>
2019-10-31 01:24:34 +0800

    Update sahw2-1.sh

--- HW2/sahw2-1.sh
+++ HW2/sahw2-1.sh
@@ -4,6 +4,5 @@ sort -k 5 -r -n |
 awk 'BEGIN{ count=0; dirnum = 0; filenum = 0; totalsize =0; } 
 /^-/{ if (count++ < 5) print count,":",$5 ,$9 } 
 /^d/{dirnum++ } 
-/^-/{filenum++} 
-{totalsize += $5} 
+/^-/{filenum++;totalsize += $5;} 
 END{ print "Dir num:"dirnum"\n" "File num:"filenum"\n" "Total : " totalsize }'
